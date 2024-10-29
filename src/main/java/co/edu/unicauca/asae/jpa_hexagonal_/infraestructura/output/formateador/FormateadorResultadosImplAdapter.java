68064fae180ee05d7a3da72920328f715e431486
Sebastian Bonilla <jsbonilla@unicauca.edu.co>
2024-10-29 01:59:20 -0500

    falta implementar formateador

--- src/main/java/co/edu/unicauca/asae/jpa_hexagonal_/infraestructura/output/formateador/FormateadorResultadosImplAdapter.java
+++ src/main/java/co/edu/unicauca/asae/jpa_hexagonal_/infraestructura/output/formateador/FormateadorResultadosImplAdapter.java
@@ -1,1 +1,22 @@
 package co.edu.unicauca.asae.jpa_hexagonal_.infraestructura.output.formateador;
+
+import org.springframework.stereotype.Service;
+
+import co.edu.unicauca.asae.jpa_hexagonal_.aplicacion.output.LocationFormateadorResultadosIntPort;
+
+@Service
+public class FormateadorResultadosImplAdapter implements LocationFormateadorResultadosIntPort {
+
+    @Override
+    public void retornarRespuestaErrorEntidadExiste(String mensaje) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void retornarRespuestaErrorReglaDeNegocio(String mensaje) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
--- src/main/java/co/edu/unicauca/asae/jpa_hexagonal_/infraestructura/output/persistencia/repositorios/LocationRepository.java
+++ src/main/java/co/edu/unicauca/asae/jpa_hexagonal_/infraestructura/output/persistencia/repositorios/LocationRepository.java
@@ -8,6 +8,6 @@ import co.edu.unicauca.asae.jpa_hexagonal_.dominio.modelos.Location;
 
 public interface LocationRepository extends JpaRepository<LocationEntity, Integer> {
 
-    @Query("SELECT count(*) FROM LocationEntity p  WHERE p.codigo=?1")
-    Integer existeLocationPorCodigo(String codigo);
+    @Query("SELECT count(*) FROM LocationEntity p  WHERE p.id=?1")
+    Integer existeLocationPorCodigo(String id);
 }
