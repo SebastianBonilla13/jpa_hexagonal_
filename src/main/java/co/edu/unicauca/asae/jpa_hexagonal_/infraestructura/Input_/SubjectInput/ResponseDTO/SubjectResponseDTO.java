ee9fbabb7168e5e182cde9eaca12f9bb50062607
dmesa <dm31284@gmail.com>
2024-10-31 19:02:15 -0500

    subject response DTO

--- src/main/java/co/edu/unicauca/asae/jpa_hexagonal_/infraestructura/Input_/SubjectInput/ResponseDTO/SubjectResponseDTO.java
+++ src/main/java/co/edu/unicauca/asae/jpa_hexagonal_/infraestructura/Input_/SubjectInput/ResponseDTO/SubjectResponseDTO.java
@@ -1,4 +1,15 @@
 package co.edu.unicauca.asae.jpa_hexagonal_.infraestructura.Input_.SubjectInput.ResponseDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubjectResponseDTO {
+    private String name;
+    private String codigo;
 }
